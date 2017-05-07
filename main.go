0966bc23443b6719c7a17675f5b9c419e8b51379
afrometal <radek.kwlsk@gmail.com>
2017-05-07 13:02:53 +0200

    final parameters, correct random indexes evaluation

--- main.go
+++ main.go
@@ -16,30 +16,37 @@ import (
 var debugFlag = flag.Bool("d", false, "print debug info")
 
 const (
-	iterations        = 1000000 // how many times to iterate
-	meltingpFraction  = 0.7     // fraction of melting point for starting temperature
-	targetTemperature = 0.01    // fraction of melting point for ending temperature
-	stagnantFactor    = 0.05    // fraction of stagnant iterations allowed before reheating
+	minItrs     = 5000000 // how many times to iterate (minimum)
+	meltPointF  = 0.7    // fraction of melting point for starting temperature
+	targetTempF = 0.01   // fraction of melting point for ending temperature
+	stagItrsF   = 0.1    // fraction of stagnant minItrs allowed before reheating
 )
 
 var (
-	k            int
-	n            int
-	meltingPoint float64
-	t0, t        float64
-	route        *tsp.Route
+	n, k      int
+	meltPoint float64
+	t0, t     float64
+	route     *tsp.Route
 )
 var m *tsp.Map
 var optimum *tsp.Route
 
+func randRange(from, to int) int {
+	if from > to {
+		panic("Range can't be negative")
+	}
+	return rand.Intn(to-from+1) + from
+}
+
 func iterate(t float64, x *tsp.Route) *tsp.Route {
 	y := x.GetCopy()
-again:
-	t1 := rand.Intn(n-1) + 1
-	t2 := (t1+rand.Intn(k))%(n-2) + 1
-	if t2-t1 <= 1 {
-		goto again
-	}
+	//again:
+	t1 := randRange(1, n-3)
+	//t2 := (t1+rand.Intn(k))%(n-2) + 1
+	t2 := randRange(t1+2, n-1)
+	//if t2-t1 <= 1 {
+	//	goto again
+	//}
 
 	y.Swap(m, t1, t2)
 
@@ -54,12 +61,12 @@ again:
 
 func iterateP(p float64, x *tsp.Route) *tsp.Route {
 	y := x.GetCopy()
-again:
-	t1 := rand.Intn(n-1) + 1
-	t2 := (t1+rand.Intn(k))%(n-2) + 1
-	if t2-t1 <= 1 {
-		goto again
-	}
+	//again:
+	t1 := randRange(1, n-3)
+	t2 := randRange(t1+2, int(math.Min(float64(t1+k), float64(n-1))))
+	//if t2-t1 <= 1 {
+	//	goto again
+	//}
 
 	y.Swap(m, t1, t2)
 
@@ -89,7 +96,8 @@ func main() {
 	m = tsp.ParseFrom(scanner)
 
 	n = m.Size()
-	k = int(math.Max(0.05*float64(n), 2.0))
+	// k = n
+	k = int(math.Max(0.01*float64(n), 25.0))
 
 	route = tsp.InitialRoute(m)
 
@@ -98,44 +106,50 @@ func main() {
 	log.Printf("Initial cost: %.5f\n", route.Cost)
 
 	testRoute := route.GetCopy()
-	var minf, maxf float64
+	var minT, maxT float64
 	var i int
-	for i, maxf, minf = 0, 0, math.Pow(10.0, 10.0); i < int(math.Max(0.01*float64(iterations), 2.0)); i++ {
+
+	for i, maxT, minT = 0, 0, math.Pow(10.0, 10.0); i < int(math.Max(0.01*float64(minItrs), 2.0)); i++ {
 		testRoute = iterateP(0.001, testRoute)
-		if testRoute.Cost < minf {
-			minf = testRoute.Cost
+		if testRoute.Cost < minT {
+			minT = testRoute.Cost
 		}
-		if testRoute.Cost > maxf {
-			maxf = testRoute.Cost
+		if testRoute.Cost > maxT {
+			maxT = testRoute.Cost
 		}
 	}
 
-	meltingPoint = (maxf - minf) * math.Pow(10.0, -1.0*math.Log10(float64(n)))
+	meltPoint = (maxT - minT) * math.Pow(10.0, -1.0*math.Log10(float64(n)))
 
-	t0 = meltingPoint * meltingpFraction
-	//t0 = 100
+	t0 = meltPoint * meltPointF
+	// t0 = 50
 	log.Printf("T0 = %.2f\n", t0)
 	t = t0
 
-	tDecay := math.Pow(targetTemperature, 1.0/iterations)
+	tDecay := math.Pow(targetTempF, 1.0/minItrs)
 
-	var l, optItr = 1, 1
-	for ; l < iterations; l++ {
+	var itr, optItr, stagItrs = 1, 1, 0
+	for ; t > t0*targetTempF; itr++ {
 		route = iterate(t, route)
 		if route.Cost < optimum.Cost {
 			optimum = route.GetCopy()
-			optItr = l
+			optItr = itr
+		} else {
+			stagItrs++
 		}
 
-		if l-optItr == int(stagnantFactor*iterations) {
-			optItr = l
+		if stagItrs == int(stagItrsF*minItrs) {
+			stagItrs = 0
 			log.Println("reheating")
-			t *= 1.0 + 0.5*((float64(iterations)-float64(l))/float64(iterations))
+			t *= 1.0 + 0.7*((float64(minItrs)-float64(itr))/float64(minItrs))
+
+			// tDecay = math.Pow(targetTempF, 1.0/float64(minItrs-itr))
 			//route = optimum.GetCopy()
 		}
 
 		t *= tDecay
-		if l%(iterations/10) == 0 {
+
+		if *debugFlag && itr%(minItrs/5) == 0 {
 			log.Printf("T  = %.2f\n", t)
 			log.Printf("cost:\t%.5f\n", route.Cost)
 			log.Printf("optimum:\t%.5f\n", optimum.Cost)
@@ -149,11 +163,6 @@ func main() {
 	log.Printf("T  = %.2f\n", t)
 	log.Printf("Optimum: %.5f\n", optimum.Cost)
 	log.Printf("%.2f%% improvement\n", 100.0-100*optimum.Cost/initialCost)
-	log.Printf("after %.2f%% of iterations\n", float64(100*optItr)/float64(iterations))
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+	log.Printf("after %.2f%% of iterations\n", float64(100*optItr)/float64(itr))
+	log.Printf("done %.2f%% of minimum iterations\n", float64(100*itr)/float64(minItrs))
 }
